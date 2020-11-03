9106c73509f790639f1278a43202dce8fe5e4f14
JosileyPraxedes <56873688+JosileyPraxedes@users.noreply.github.com>
2020-11-03 17:05:35 -0300

    refactoring

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,99 +1,93 @@
 //Cadastro de endereço
 function populateUFs() {
-    const ufSelect = document.querySelector("select[name=uf]")
+  const ufSelect = document.querySelector("select[name=uf]");
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-        .then(res => res.json())
-        .then(states => {
-
-            for(const state of states) {
-                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-            }
-        })
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res) => res.json())
+    .then((states) => {
+      for (const state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
 }
 
-populateUFs()
+populateUFs();
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=state]")
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("input[name=state]");
 
-    const ufValue = event.target.value
+  const ufValue = event.target.value;
 
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState].text
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-    citySelect.innerHTML = "<option value> Selecione a Cidade</option>"
-    citySelect.disabled = true
+  citySelect.innerHTML = "<option value> Selecione a Cidade</option>";
+  citySelect.disabled = true;
 
-    fetch(url)
-    .then(res => res.json())
-    .then(cities => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((cities) => {
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
+      }
 
-        for(const city of cities) {
-            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-        }
-        
-        citySelect.disabled = false
-    })
+      citySelect.disabled = false;
+    });
 }
 
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
-
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
 
 //Itens de coleta
-//pega todos os li. 
+//pega todos os li.
 //Cada um item adiciona uma callback para ser executada toda vez que for clicada
-const itemsToCollect = document.querySelectorAll(".items-grid li")
+const itemsToCollect = document.querySelectorAll(".items-grid li");
 
 for (const item of itemsToCollect) {
-    item.addEventListener("click", handleSelectedItem)
+  item.addEventListener("click", handleSelectedItem);
 }
 
 //pegando items de coleta
 //atualizar o campo escondido com os itens selecionados parte 1
-const collectedItems = document.querySelector("input[name=items]")
+const collectedItems = document.querySelector("input[name=items]");
 
 //colocando os valores
-let selectItems = []
+let selectItems = [];
 
 function handleSelectedItem(event) {
-    const itemLi = event.target
-    
-    //adicionar ou remover uma classe com javascript
-    itemLi.classList.toggle("selected")
+  const itemLi = event.target;
 
-    const itemId = event.target.dataset.id
+  //adicionar ou remover uma classe com javascript
+  itemLi.classList.toggle("selected");
 
-    console.log('ITEM ID: ', itemId)
-    
-    //verificando se existe itens selecionados, se sim
-    //pegar os itens selecionados
-    const alreadySelected = selectItems.findIndex( item =>  {
-        const itemFound = item == itemId //verdadeiro ou falso
-        return itemFound
-    })
+  const itemId = event.target.dataset.id;
 
-    //se já estiver selecionado, tirar da seleção
-    if( alreadySelected >= 0 ){
-        //tirando a seleção
-        const filteredItems = selectItems.filter( item =>{
-            const itemIsDiferent = item != itemId
-            return itemIsDiferent
-        })
-        selectItems = filteredItems
-    } else { 
-        //se não tiver selecionado, adicionar à seleção
-        selectItems.push(itemId)
-    }
+  console.log("ITEM ID: ", itemId);
 
-    console.log('ITEM ID: ', itemId)
+  //verificando se existe itens selecionados, se sim
+  //pegar os itens selecionados
+  const alreadySelected = selectItems.findIndex((item) => {
+    const itemFound = item == itemId; //verdadeiro ou falso
+    return itemFound;
+  });
 
-    //atualizar o campo escondido com os itens selecionados parte 2
-    collectedItems.value = selectItems
+  //se já estiver selecionado, tirar da seleção
+  if (alreadySelected >= 0) {
+    //tirando a seleção
+    const filteredItems = selectItems.filter((item) => {
+      const itemIsDiferent = item != itemId;
+      return itemIsDiferent;
+    });
+    selectItems = filteredItems;
+  } else {
+    //se não tiver selecionado, adicionar à seleção
+    selectItems.push(itemId);
+  }
 
+  console.log("ITEM ID: ", itemId);
+
+  //atualizar o campo escondido com os itens selecionados parte 2
+  collectedItems.value = selectItems;
 }
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,18 +1,18 @@
 //pegando o botão
-const buttonSearch = document.querySelector("#page-home main a")
+const buttonSearch = document.querySelector("#page-home main a");
 
 //quando clicar no botão search, pegar o modal
-const modal = document.querySelector("#modal")
+const modal = document.querySelector("#modal");
 
-//quando clicar no 'a' 
-const close = document.querySelector("#modal .header a")
+//quando clicar no 'a'
+const close = document.querySelector("#modal .header a");
 
 //quando clicar no botão, um evento de click de remover o hide
-buttonSearch.addEventListener("click", () =>{
-    modal.classList.toggle("hide")
-})
+buttonSearch.addEventListener("click", () => {
+  modal.classList.toggle("hide");
+});
 
 //quando usar o closeadd executa uma função anonima que chama o modal e adiciona a lista de classes e adicionar o hide
 close.addEventListener("click", () => {
-    modal.classList.add("hide")
-})
+  modal.classList.add("hide");
+});
